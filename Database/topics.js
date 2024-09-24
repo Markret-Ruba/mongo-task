05da34a612c6ff4a9ee3dc457969b338ee0cd63b
MARKRET-RUBA <markretruba234@gmail.com>
2024-09-24 14:54:57 +0530

    first commit

---
+++ Database/topics.js
@@ -0,0 +1,36 @@
+[
+{
+    "topic":"Demo class",
+    "month":"october",
+    "task": "basic of js"
+},
+{
+    "topic":"javascript_Day1",
+    "month":"october",
+    "task":"functions"
+},
+{
+    "topic":"javascript_Day2",
+    "month":"october",
+    "task":"for loops"
+},
+{
+    "topic":"javascript_Day3",
+    "month":"october",
+    "task":"Oops"
+},
+{
+    "topic":"html_Day1",
+    "month":"november",
+    "task":"blog about html"
+},
+{
+    "topic":"html_Day2",
+    "month":"november",
+    "task":"elements/tags"
+},
+{
+    "topic":"html_Day3",
+    "month":"november",
+    "task":"inputs"
+}]
